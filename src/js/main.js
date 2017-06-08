5aae2825ff8030656ac38f401bf5d4feed43cf61
潘星宇 <421915293@163.com>
2017-06-08 15:26:40 +0800

    remove debug tags

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,26 @@
 function selectInit() {
     for(var i in iName) {
         var s = '<option value="' + iName[i] + '">' + iName[i] + '</option>';
-        $('#newInsType').append(s);
+        $('#new-instruction-type-selector').append(s);
         $('#editInsType').append(s);
     }
     for(var i = 0; i < FU_SIZE; i++) {
         $('#editFloatRegister').append('<option value="'+i+'">F'+i+'</option>');
     }
-    $('.selectpicker').selectpicker();
-
+    // $('.selectpicker').selectpicker();
 }
+
+
+$(document).ready(function () {
+    selectInit();
+    // updateBus(bus);
+    // redrawMem(bus.memory, memStart, memNum);
+    // bus.addInst(new Instruction(iName.addd, 1, 2, 3));
+    // bus.addInst(new Instruction(iName.subd, 2, 1, 2));
+    // bus.addInst(new Instruction(iName.st, 2, 0, 0));
+    // updateBus(bus);
+    // redrawMem(bus.memory, memStart, memNum);
+    // /*bus.plusOneSecond();
+    //  updateBus(bus,instList);
+    //  redrawMem(bus.memory, memStart, memNum);*/
+});
